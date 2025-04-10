9e53088d5b58aee2975a7b65bb130e3dc0a92e60
Alex Kiefer <alexkiefer51@gmail.com>
2025-04-10 09:53:53 -0400

    Added some documentation to the rocm_profiler as part of debuggin metric logging

--- utils/rocm_profiler.py
+++ utils/rocm_profiler.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Dict, Any, Tuple
 ROCM_SMI_PATH = os.environ.get("ROCM_SMI_PATH", None)
 if ROCM_SMI_PATH is not None:
     sys.path.append(ROCM_SMI_PATH)
@@ -11,7 +12,19 @@ class RocmProfiler:
     def __init__(self):
         rocm_smi.initializeRsmi()
 
-    def getPower(self, device):
+    def getPower(self, device) -> Dict[str, Any]:
+        """Gets the power usage of a given GPU.
+
+        Args:
+            device (int): The device index to get the power usage for.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the power usage of the given GPU with the following keys:
+                "power": float,
+                "power_type": str,
+                "unit": str,
+                "ret": int
+        """
         return rocm_smi.getPower(device)
 
     def listDevices(self):
@@ -21,10 +34,19 @@ class RocmProfiler:
         (memUsed, memTotal) = rocm_smi.getMemInfo(device, "vram")
         return round(float(memUsed)/float(memTotal) * 100, 2)
     
-    def getUtilization(self, device):
+    def getUtilization(self, device) -> float:
         return rocm_smi.getGpuUse(device)
     
-    def getTemp(self, device, sensor=None):
+    def getTemp(self, device, sensor=None) -> Tuple[str, float]:
+        """Gets the temperature of a given GPU.
+
+        Args:
+            device (int): The device index to get the temperature for.
+            sensor (str, optional): The sensor to get the temperature for. Defaults to None.
+
+        Returns:
+            Tuple[str, float]: A tuple containing the sensor name and the temperature in Celsius.
+        """
         if sensor is None:
             return rocm_smi.findFirstAvailableTemp(device)
         else:
